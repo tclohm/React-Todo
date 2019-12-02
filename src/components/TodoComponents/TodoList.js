f36a00b7065df2156c40ee104f3782f7e9bdbc55
Taylor Lohman <taylorlohman@Taylors-MBP.lan1>
2019-12-02 15:23:42 -0800

    added all handlers to the App.js and passed props down to the components

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,28 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+import Todo from "./Todo";
+
+import "./Todo.css";
+
+const TodoList = (props) => {
+
+	return (
+		<div className="todo-list">
+			{(props.tasks.length === 0) ?
+
+				<h6>No Tasks</h6>
+
+				:
+
+				props.tasks.map(task => (
+
+				<Todo key={task.id} task={task} toggleCompleted={props.toggleCompleted} />
+
+			))}
+		</div>
+	);
+};
+
+export default TodoList;
